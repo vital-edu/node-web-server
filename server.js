a29bb7b64111dd67780faaba0181d153fb19362a
Eduardo Vital <vitaldu@gmail.com>
2016-11-27 20:28:28 -0200

    Adding middlewares to save logs.
    Signed-off-by: Eduardo Vital <vitaldu@gmail.com>

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  var date = new Date().toString();
+  var log = `${date}: ${req.method} ${req.url}`
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (error) => {
+    if (error) {
+      console.log('Unable to write in server.log.');
+      console.log('Error: ' + error);
+    }
+  });
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('screamIt', (text) => {
